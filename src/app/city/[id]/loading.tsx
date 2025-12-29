487cf76946c3f83cb1bf28487ca355d11927bfda
Iutrera19 <ignacio.utrera274@gmail.com>
2025-12-29 17:39:45 -0300

    Deleted old unused files and pages

--- src/app/city/[id]/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (<>
-    <p className="text-2xl font-semibold text-white mb-2">Cargando datos de la ciudad...</p>
-    <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-white"></div>
-  </>)
-
-}
